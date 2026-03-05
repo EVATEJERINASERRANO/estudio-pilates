40d57883bd4b07690a800e8d15e56c7930261a71
Eva Tejerina Serrano <evaact@hotmail.com>
2026-03-05 16:41:36 +0100

    Creación del proyecto backend con Spring Boot

---
+++ src/main/java/com/pilates/estudiopilates/EstudioPilatesApplication.java
@@ -0,0 +1,13 @@
+package com.pilates.estudiopilates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudioPilatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudioPilatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pilates/estudiopilates/EstudioPilatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilates.estudiopilates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudioPilatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
